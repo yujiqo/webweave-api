a2e4cad30c2add89aaed148fb967b66027b4764b
August <74107318+August-sudo@users.noreply.github.com>
2023-06-13 22:38:42 +0600

    fixed typo in server.py
    instanse was renamed into instance

--- app/server.py
+++ app/server.py
@@ -3,14 +3,14 @@ from typing import Self, Optional, Any
 
 
 class Server:
-    __instanse: Optional[Self] = None
+    __instance: Optional[Self] = None
     _server: FastAPI
 
     def __new__(cls) -> Self:
-        if cls.__instanse is None:
-            cls.__instanse = super(Server, cls).__new__(cls)
+        if cls.__instance is None:
+            cls.__instance = super(Server, cls).__new__(cls)
 
-        return cls.__instanse
+        return cls.__instance
 
     def __init__(self) -> None:
         self._server = FastAPI()
