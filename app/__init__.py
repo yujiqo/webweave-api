29a827c423e02b8a7a522ff917a654e3bee98df6
Danil <danilastolyarov97@gmail.com>
2023-06-18 19:29:18 +0600

    feat(youtube-api): add /info route

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 
 from .server import Server
-from .config import setup_app
+from .config import setup_app, routers
 
 
 def server(_=None) -> FastAPI:
     app = Server()
 
     setup_app(env=app.env)
+    app.register_routers(routers)
 
     return app.server
